08be48ac19d08311f8ac9a5741d518e78832811c
eulersantana <eulersantana@eulersantana-HP-Pavilion-dv7-Notebook-PC>
2013-07-10 16:47:03 -0300

    BD

---
+++ RoomsManagerAdmin/src/br/com/ufba/roomsmanageradmin/bean/CadastraSalaBean.java
@@ -0,0 +1,36 @@
+package br.com.ufba.roomsmanageradmin.bean;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.ufba.roomsmanageradmin.dao.CadastraSalaDAO;
+import br.com.ufba.roomsmanageradmin.model.Sala;
+
+@ManagedBean
+@ViewScoped
+public class CadastraSalaBean implements Serializable{
+	private static final long serialVersionUID = -6735027036534961738L;
+	private Sala sala = new Sala();
+	
+	public void cadastra(){
+		System.out.println("INSERT INTO sala (nome,quadro,retrojetor,multimidia,tipo,ar_condicionado,computadores,capacidade,setor_id)"+
+				 "VALUES("+sala.getNome()+",'"+sala.isQuadro()+"','"+sala.isRetroprojetor()+"','"+sala.isMultimidia()+"','"+sala.getTipo()+
+				 "','"+sala.isArcondicionado()+"','"+sala.getComputadores()+"','"+sala.getCapacidade()+"','"+sala.getSetor_id()+"')");
+		CadastraSalaDAO salaDAO = new CadastraSalaDAO();
+		
+		try {
+			salaDAO.salva(sala);
+		} catch (SQLException e) {
+			System.out.println("#"+e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Sala getSala(){
+		return sala;
+	}
+}
