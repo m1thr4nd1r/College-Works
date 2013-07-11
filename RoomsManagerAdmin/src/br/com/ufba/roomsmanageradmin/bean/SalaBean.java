c9085a6a2de7e6d877ee9443aff831b3f3ac2dcf
eulersantana <eulersantana@eulersantana-HP-Pavilion-dv7-Notebook-PC>
2013-07-11 14:34:37 -0300

    Deleção CadastraSalaBean e Criação de Lista Sala

--- RoomsManagerAdmin/src/br/com/ufba/roomsmanageradmin/bean/SalaBean.java
+++ RoomsManagerAdmin/src/br/com/ufba/roomsmanageradmin/bean/SalaBean.java
@@ -2,9 +2,13 @@ package br.com.ufba.roomsmanageradmin.bean;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.swing.JOptionPane;
+
+import org.eclipse.jdt.internal.compiler.ast.Literal;
 
 import br.com.ufba.roomsmanageradmin.dao.SalaDAO;
 import br.com.ufba.roomsmanageradmin.model.Sala;
@@ -13,6 +17,7 @@ import br.com.ufba.roomsmanageradmin.model.Sala;
 @ViewScoped
 public class SalaBean implements Serializable{
 	private static final long serialVersionUID = -6735027036534961738L;
+	private static ArrayList<Sala> listaSalas = new ArrayList<Sala>();
 	private Sala sala = new Sala();
 	
 	public void cadastra(){
@@ -23,6 +28,7 @@ public class SalaBean implements Serializable{
 		
 		try {
 			salaDAO.salva(sala);
+			lista();
 		} catch (SQLException e) {
 			System.out.println("#"+e.getMessage());
 			e.printStackTrace();
@@ -30,7 +36,22 @@ public class SalaBean implements Serializable{
 		
 	}
 	
+	public void lista(){
+		SalaDAO salaDAO = new SalaDAO();
+		try{
+			salaDAO.listaSala(listaSalas);
+		}catch(SQLException e){
+		
+		}
+		
+	}
+	
+	
 	public Sala getSala(){
 		return sala;
+	}
+	
+	public ArrayList<Sala> getListaSalas(){
+		return listaSalas;
 	}
 }
