2c03c13199378046b2fb0699fbe4a8e0530308e6
Victor <vitocr_santos@hotmail.com>
2013-07-18 18:52:50 -0300

    bean e model

---
+++ src/main/java/com/bean/UsuarioBean.java
@@ -0,0 +1,49 @@
+package com.bean;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.event.ActionEvent;
+import javax.swing.JOptionPane;
+
+import com.model.Usuario;
+
+//@ManagedBean
+public class UsuarioBean implements Serializable{
+		
+	private Usuario usuario = new Usuario();
+	private List usuarios; 
+		
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List getUsuarios(){
+		 
+		return this.usuarios;
+	}
+	
+//	public String salvar(ActionEvent ae) throws ParseException
+	public String salvar()
+	{
+//		try	{
+//				userDAO.salva(usuario);
+//				
+//				return "index";
+//			
+//			} catch (SQLException e) {
+//				System.out.println("ERRO: "+e.getMessage());
+//				e.printStackTrace();
+//			}
+//		
+		return "reserva";
+	}
+}
---
+++ src/main/java/com/model/Usuario.java
@@ -0,0 +1,65 @@
+package com.model;
+
+public class Usuario {
+	
+	private int num_cadastro;
+	private String nome, senha, email;
+	private boolean tipo;
+	
+	public Usuario()
+	{
+		this.nome = "";
+		this.senha = "";
+		this.email = "";
+		this.tipo = false;
+	}
+
+	public Usuario(String email, String senha, String nome, int num_cadastro, boolean tipo)
+	{
+		this.email = email;
+		this.senha = senha;
+		this.nome = nome;
+		this.num_cadastro = num_cadastro;
+		this.tipo = tipo;
+	}
+	
+	public boolean getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(boolean tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNum_cadastro() {
+		return num_cadastro;
+	}
+
+	public void setNum_cadastro(int num_cadastro) {
+		this.num_cadastro = num_cadastro;
+	}
+}
