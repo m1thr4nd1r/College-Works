7f1d465ce903c8c6614d9d4f642cd278fd6cf29d
unknown <M1thr4nd1r@BLIZZARD.(none)>
2013-07-28 22:07:08 -0300

    Adicionando o Login e index do projeto original

---
+++ src/main/java/br/com/ufba/roomsmanageradmin/controller/controller.java
@@ -0,0 +1,50 @@
+package br.com.ufba.roomsmanageradmin.controller;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.swing.JOptionPane;
+
+import br.com.ufba.roomsmanageradmin.model.Login;
+
+public class LoginController extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	private HttpSession session;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		PrintWriter out = response.getWriter();
+		
+		String user = request.getParameter("username");
+		String senha = request.getParameter("senha");
+		
+		Login login = new Login(user,senha);
+		session = request.getSession(true);
+		
+		if(login.validate()){
+			session.setAttribute("logado", user);
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "Credenciais incorretas.");
+		}
+		
+		out.println("<script>location.href='"+request.getRequestURL()+"/../index.jsp';</script>");
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		PrintWriter out = response.getWriter();
+		session = request.getSession();
+		int conf = JOptionPane.showConfirmDialog(null,"Deseja sair do sistema ?","RoomsManager",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+		
+		if(conf == 0){
+			session.invalidate();
+			out.println("<script>location.href='"+request.getRequestURL()+"/../index.jsp';</script>");
+		}
+		else{
+			out.println("<script>window.history.back()</script>");
+		}
+		
+	}
+	
+}
