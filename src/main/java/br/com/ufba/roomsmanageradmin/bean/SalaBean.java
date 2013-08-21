bd8075a2d2ad75a6ea57913e67bbd7f9e618c7ff
eulersantana <euler-m.d@hotmail.com>
2013-08-21 10:41:42 -0300

    Lista Sala

--- src/main/java/br/com/ufba/roomsmanageradmin/bean/SalaBean.java
+++ src/main/java/br/com/ufba/roomsmanageradmin/bean/SalaBean.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 
 import org.primefaces.event.RowEditEvent;
@@ -42,6 +43,7 @@ public class SalaBean implements Serializable{
 	private static final long serialVersionUID = -6735027036534961738L;
 	private static ArrayList<Sala> listaSalas = new ArrayList<Sala>();
 	private ArrayList<Setor> listaSetor = new ArrayList<Setor>();
+	private DataModel<Sala> sal;
 	private Sala sala = new Sala();
 	private String setor_id;
 	
@@ -83,9 +85,58 @@ public class SalaBean implements Serializable{
 	    }
 	    return "list?faces-redirect=true";
     }
-
 	
-	 
+	public String update(RowEditEvent event) throws ParseException
+	{
+		SessionFactory sf = Hibernate.getSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = null;
+		
+		sala = (Sala) event.getObject();
+		
+		System.out.println("Chego");
+		
+		try{
+			tx = session.beginTransaction();
+			session.update(sala);
+			session.flush();
+			tx.commit(); 
+		}catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+				e.printStackTrace(); 
+		}finally {
+		session.close(); 
+		}
+		
+		return "list"; 
+	}
+	
+	
+	public void delete()
+    {
+		SessionFactory sf = Hibernate.getSessionFactory();
+	    Session session = sf.openSession();
+	    Transaction tx = null;
+	
+	    select();
+	    
+	    try{
+	    	tx = session.beginTransaction();
+	    	session.delete(sala); 
+	    	tx.commit();
+    	}catch (HibernateException e) {
+    		if (tx!=null) tx.rollback();
+	    	e.printStackTrace(); 
+    	}finally {
+	    	session.close();
+	    }
+    }
+	
+	public void select()
+	{
+        this.sala = this.sal.getRowData();
+        JOptionPane.showMessageDialog(null, sala.getNome());
+    }
 	 private String getRandomModel() {  
 	        return UUID.randomUUID().toString().substring(0, 8);  
 	 }
@@ -93,7 +144,7 @@ public class SalaBean implements Serializable{
 	 public void onEdit(RowEditEvent event) {  
 	        FacesMessage msg = new FacesMessage("Sala editada", ((Sala) event.getObject()).getNome());  
 	        Sala sala = (Sala) event.getObject();
-//	        Sala user = Facade.find(use.getId()); 
+	        //Sala user = Facade.find(use.getId()); 
 	        System.out.print(sala.getNome()+" "+sala.getSetorNome()+"\n");
 	        FacesContext.getCurrentInstance().addMessage(null, msg);  
 	}  
@@ -121,7 +172,9 @@ public class SalaBean implements Serializable{
 		return listaSalas;
 	}
 
-	
+	public DataModel<Sala> getSal(){		    
+	    return sal;
+	}
 	public ArrayList<Setor> getListaSetor(){
 		return listaSetor;
 	}
