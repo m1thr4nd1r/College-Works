12dcf1e69ac3487ec77bec21cd3099b2374b8452
Victor <vitocr_santos@hotmail.com>
2013-08-22 18:47:46 -0300

    Hibernate do webservice

--- src/main/java/br/com/ufba/roomsmanageradmin/services/ReservaSalaService.java
+++ src/main/java/br/com/ufba/roomsmanageradmin/services/ReservaSalaService.java
@@ -10,7 +10,12 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import br.com.ufba.roomsmanageradmin.dao.ReservaSalaDAO;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+// import br.com.ufba.roomsmanageradmin.dao.ReservaSalaDAO;
 import br.com.ufba.roomsmanageradmin.dao.Hibernate;
 import br.com.ufba.roomsmanageradmin.model.Sala;
 import br.com.ufba.roomsmanageradmin.model.ReservaSala;
@@ -21,17 +26,21 @@ public class ReservaSalaService {
 
 	@GET
 	public ReservaSala [] get(@PathParam("data") String data){
-		ReservaSalaDAO dao = new ReservaSalaDAO();
+		// ReservaSalaDAO dao = new ReservaSalaDAO();
 		ReservaSala [] reservas = null;
 		try {
-			List lista = dao.getReservas(data);
+			// List lista = dao.getReservas(data);
+			SessionFactory sf = Hibernate.getSessionFactory();
+	    	Session session = sf.openSession();
+
+	    	List<ReservaSala> lista = (List<ReservaSala>) session.createQuery("FROM ReservaSala").list();
 			reservas = new ReservaSala[lista.size()];
 			int i = 0;
-			for (ReservaSala reserva : dao.getReservas(data)) {
+			for (ReservaSala reserva : (List<ReservaSala>) session.createQuery("FROM ReservaSala").list()) {
 				reservas[i] = reserva;
 				i++;
 			}
-		} catch (SQLException e) {
+		} catch (HibernateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- src/main/java/br/com/ufba/roomsmanageradmin/services/SalaService.java
+++ src/main/java/br/com/ufba/roomsmanageradmin/services/SalaService.java
@@ -9,8 +9,14 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import br.com.ufba.roomsmanageradmin.dao.SalaDAO;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+// import br.com.ufba.roomsmanageradmin.dao.SalaDAO;
 import br.com.ufba.roomsmanageradmin.model.Sala;
+import br.com.ufba.roomsmanageradmin.dao.Hibernate;
 
 @Path("/salas")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,13 +24,14 @@ public class SalaService {
 	
 	@GET
 	public String [][] get(){
-		SalaDAO dao = new SalaDAO();
+		// SalaDAO dao = new SalaDAO();
 		String [][] salas = null;
 		List<Sala> lista;
 		
 		try {
-			
-			lista = dao.getSalas();
+			SessionFactory sf = Hibernate.getSessionFactory();
+	    	Session session = sf.openSession();
+	    	lista = (List<Sala>) session.createQuery("FROM Sala").list();
 			salas = new String[lista.size()][2];			
 			int i = 0;
 			for (Sala sala : lista){
@@ -33,7 +40,7 @@ public class SalaService {
 				i++;
 			}
 
-		} catch (SQLException e) {
+		} catch (HibernateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
