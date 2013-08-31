077ca4d1ef27c0be86344db35fd0883dfd3a6880
unknown <M1thr4nd1r@BLIZZARD.(none)>
2013-08-31 00:49:45 -0300

    Removing files

--- run.sh
+++
@@ -1,2 +0,0 @@
-mvn package
-java -jar target/dependency/webapp-runner.jar target/*.war
--- src/main/webapp/js/template.js
+++
@@ -1,5 +0,0 @@
-function acesso_pessoa_setor(){
-	var template =  "<form>" +
-						"<input type=\"text\" id='pessoa_matr'"+
-					"</form>";
-}
