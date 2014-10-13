055a37921a723353fa2d7079408c1eaf6c63b28a
Victor dos Santos <vitocr_santos@hotmail.com>
2014-10-12 22:16:26 -0300

    Atualizando arquivos

--- trabalho1/Victor/test.sh
+++ trabalho1/Victor/test.sh
@@ -1,4 +1,5 @@
-gcc -g lexico.c
+cd trabalho1/Victor/
+
 ./a.out ../Entradas/sample1.in > sample1.sol
 ./a.out ../Entradas/sample2.in > sample2.sol
 ./a.out ../Entradas/sample3.in > sample3.sol
@@ -21,47 +22,48 @@ gcc -g lexico.c
 ./a.out ../Entradas/sala3.in > sala3.sol
 ./a.out ../Entradas/sala4.in > sala4.sol
 ./a.out ../Entradas/sala5.in > sala5.sol
+
 echo "Sample 1"
 diff sample1.sol ../Saidas/sample1.out
 echo "Sample 2"
 diff sample2.sol ../Saidas/sample2.out
 echo "Sample 3"
-diff sample3.sol ../Saidas/sample3.v
+diff sample3.sol ../Saidas/sample3V.out
 echo "Sample 4"
-diff sample4.sol ../Saidas/sample4.v
+diff sample4.sol ../Saidas/sample4V.out
 echo "Sample 5"
-diff sample5.sol ../Saidas/sample5.v
+diff sample5.sol ../Saidas/sample5V.out
 echo "Sample 6"
-diff sample6.sol ../Saidas/sample6.v
+diff sample6.sol ../Saidas/sample6V.out
 echo "Sample 7"
-diff sample7.sol ../Saidas/sample7.v
+diff sample7.sol ../Saidas/sample7V.out
 echo "Sample 8"
-diff sample8.sol ../Saidas/sample8.v
+diff sample8.sol ../Saidas/sample8V.out
 echo "Sample 9"
-diff sample9.sol ../Saidas/sample9.v
+diff sample9.sol ../Saidas/sample9V.out
 echo "Sample 10"
-diff sample10.sol ../Saidas/sample10.v
+diff sample10.sol ../Saidas/sample10V.out
 echo "Samples"
-diff samples.sol ../Saidas/samples.v
+diff samples.sol ../Saidas/samplesV.out
 echo "Strings"
-diff strings.sol ../Saidas/strings.v
+diff strings.sol ../Saidas/stringsV.out
 echo "Numbers"
-diff numbers.sol ../Saidas/numbers.v
+diff numbers.sol ../Saidas/numbersV.out
 echo "Char"
-diff char.sol ../Saidas/char.v
+diff char.sol ../Saidas/charV.out
 echo "Separators"
-diff separators.sol ../Saidas/separators.v
+diff separators.sol ../Saidas/separatorsV.out
 echo "Teste"
 diff test.sol ../Saidas/test.out
 echo "Maximos"
-diff maximos.sol ../Saidas/maximos.v
+diff maximos.sol ../Saidas/maximosV.out
 echo "Sala"
-diff sala.sol ../Saidas/sala.v
+diff sala.sol ../Saidas/salaV.out
 echo "Sala 2"
-diff sala2.sol ../Saidas/sala2.v
+diff sala2.sol ../Saidas/sala2V.out
 echo "Sala 3"
-diff sala3.sol ../Saidas/sala3.v
+diff sala3.sol ../Saidas/sala3V.out
 echo "Sala 4"
-diff sala4.sol ../Saidas/sala4.v
+diff sala4.sol ../Saidas/sala4V.out
 echo "Sala 5"
-diff sala5.sol ../Saidas/sala5.v
+diff sala5.sol ../Saidas/sala5V.out
