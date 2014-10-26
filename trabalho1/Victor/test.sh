7f1eb3c8e439e030a5495f9fd44c7a9271ccb93b
Victor dos Santos <vitocr_santos@hotmail.com>
2014-10-25 21:25:52 -0300

    Atualizando teste

--- trabalho1/Victor/test.sh
+++ trabalho1/Victor/test.sh
@@ -20,15 +20,19 @@ do
 		if [ $res -ne 0 ]; then
 			echo $name "\n" $diff
 			flag=false
+		else
+			$(rm ${saida})
 		fi
 	else
 		out="../Saidas/${name}V.out"
-		diff=`diff -q -N ${saida} ${out}`
+		diff=`diff -N ${saida} ${out}`
 		res=$?
 		# echo $out $saida $diff
 		if [ -e $out ] && [ $res -ne 0 ]; then
 			echo $name "\n" $diff
 			flag=false
+		else
+			$(rm ${saida})
 		fi
 	fi
 done
