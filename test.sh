7aad40a965c636cbb5affffa4d2ff027c8051c9d
Victor dos Santos <vitocr_santos@hotmail.com>
2014-12-05 17:17:56 -0300

    Adicionando o valgrind aos testes

--- test.sh
+++ test.sh
@@ -13,24 +13,17 @@ do
 	out="Saidas/${name}.out"
 	saida="./${name}.sol"
 
-	$(./a.out ${input} > ${saida})	
-
-	if [ -e $out ]; then
-		diff=`diff ${saida} ${out}`
-		#${diff}
-		res=$?
-		if [ $res -ne 0 ]; then
-			echo $name "\n" $diff
-			flag=false
-		else
-			$(rm ${saida})
-		fi
+	memcheck=`valgrind -q --error-exitcode=1 ./a.out ${input}` 
+	res=$?
+	if [ $res -ne 0 ]; then
+		echo $memcheck "\n"
 	else
-		out="Saidas/${name}V.out"
-		diff=`diff -N ${saida} ${out}`
-		res=$?
-		# echo $out $saida $diff
+		$(./a.out ${input} > ${saida})	
+
 		if [ -e $out ]; then
+			diff=`diff ${saida} ${out}`
+			#${diff}
+			res=$?
 			if [ $res -ne 0 ]; then
 				echo $name "\n" $diff
 				flag=false
@@ -38,9 +31,23 @@ do
 				$(rm ${saida})
 			fi
 		else
-			$(mv ${saida} ${out})
+			out="Saidas/${name}V.out"
+			diff=`diff -N ${saida} ${out}`
+			res=$?
+			# echo $out $saida $diff
+			if [ -e $out ]; then
+				if [ $res -ne 0 ]; then
+					echo $name "\n" $diff
+					flag=false
+				else
+					$(rm ${saida})
+				fi
+			else
+				$(mv ${saida} ${out})
+			fi
 		fi
 	fi
+
 done
 
 echo "\nFim do Teste"
